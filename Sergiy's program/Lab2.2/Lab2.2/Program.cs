91b0b0f2079a589396268417efbe7bcb7abec0c6
Sergiy <ipz22-s.spielov@nubip.edu.ua>
2024-07-15 22:53:56 +0300

    Added program

---
+++ Sergiy's program/Lab2.2/Lab2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input lenth of radius: ");
+            double radiusLenth = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine($"Square of the circle: {Math.Pow(radiusLenth, 2) * Math.PI}");
+            Console.WriteLine($"Perimetr of the circle: {2 * Math.PI * radiusLenth}");
+        }
+    }
+    
+}
